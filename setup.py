1a51f19405fb3a29879bdbb9f879bfec5ee5efd2
Ian McAllister <ian@mcallisternevins.com>
2016-11-25 09:46:01 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='art_utils',
+      description="Collected Utilities for Kids' Computer Art",
+      author='Ian McAllister',
+      author_email='ian@mcallisternevins.com',
+      url='https://github.com/imcallister/art_utils',
+      packages=find_packages(),
+      include_package_data=True,
+     )
